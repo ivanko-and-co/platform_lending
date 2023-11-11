a1a70f0357df235cf60637e11543f90f9a594475
HOHOL <dxd.vgv@gmail.com>
2023-11-11 16:23:33 +0500

    pay test

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from hashlib import sha256
+from typing import NamedTuple
 
-from flask import Flask, render_template, jsonify, request
+import requests
+from flask import Flask, render_template, jsonify, request, url_for
 import smtplib
 
 app = Flask(__name__)
 app.config['email'] = 'oboi@usexpo.ru'
+
+PRODUCT = {
+    1: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Штукатурка"},
+    2: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Холст"},
+    3: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Пыль"},
+    4: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Песок"},
+    5: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Мелкий лен"},
+    6: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Лоск"},
+    7: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Иней"},
+    8: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Живопись"},
+    9: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Декоративная штукатурка"},
+    10: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Smart Stick"},
+    11: {"price_size": 1599, "EXTRA": {1: {"name": "Добавить клей", "price": 59}, 2: {"name": "Латексная печать", "price": 299}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Pintura"},
+    12: {"price_size": 210, "EXTRA": {2: {"name": "Латексная печать", "price": 920}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoBOARD 'ДОСКА' (текстура гипса)"},
+    13: {"price_size": 245, "EXTRA": {2: {"name": "Латексная печать", "price": 990}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoFLAX 'ХОЛСТ' (текстура грубого холста)"},
+    14: {"price_size": 210, "EXTRA": {2: {"name": "Латексная печать", "price": 920}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoFROST 'МОРОЗ' (текстура инея)"},
+    15: {"price_size": 245, "EXTRA": {2: {"name": "Латексная печать", "price": 990}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoLINE 'ЛИНИЯ' (текстура фрески)"},
+    16: {"price_size": 210, "EXTRA": {2: {"name": "Латексная печать", "price": 920}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoMARBLE 'МРАМОР' (текстура мрамора)"},
+    17: {"price_size": 245, "EXTRA": {2: {"name": "Латексная печать", "price": 990}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoSHELL 'РАКОВИНА' (текстура мелкого песка)"},
+    18: {"price_size": 245, "EXTRA": {2: {"name": "Латексная печать", "price": 990}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "VEIKA DecoSMOOTH 'ГЛАДЬ' (гладкие)"},
+    19: {"price_size": 477, "EXTRA": {2: {"name": "Латексная печать", "price": 1455}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "SIHL 2511 Non-Woven design2wall Sol 195 Satin"},
+    20: {"price_size": 477, "EXTRA": {2: {"name": "Латексная печать", "price": 1455}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "SIHL 2512 Non-Woven design2wall Sol 195 Matt"},
+    21: {"price_size": 681, "EXTRA": {2: {"name": "Латексная печать", "price": 1899}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Magic PEBBLE Textured Vinyl Wallcovering 290g (крупный песок)"},
+    22: {"price_size": 481, "EXTRA": {2: {"name": "Латексная печать", "price": 1499}, 3: {"name": "Покрыть лаком", "price": 399}}, "name": "Magic MURAL PRO Latex-Based Wallcovering Paper (гладкие)"},
+}
+CATEGORY = {
+    1: "Бумажные обои",
+    2: "Виниловые фотообои",
+    3: "Виниловые фотообои",
+    4: "Фотообои флизелиновые (с ПВХ-покрытием)",
+    5: "Фотообои флизелиновые (без ПВХ)",
+    6: "Фотообои бумажные (с ПВХ-покрытием)",
+    7: "Фотообои из латексной бумаги (без ПВХ)",
+}
+
+
+class PayloadData(NamedTuple):
+    sum: int
+    client_email: str
+    client_phone: str
+    sign: str
+    user_result_callback: str
 
 
 @app.route('/')
@@ -21,18 +65,49 @@ def form_record():
     return jsonify(True)
 
 
-@app.route('/order', methods=['POST'])
+@app.route('/callback_url')
+def callback():
+    try:
+        app.config['result'] = {
+            'payment_id': request.args['payment_id'],
+            'clientid': request.args['clientid'],
+            'result': request.args['result']
+        }
+    except Exception as e:
+        app.config['result'] = {
+            'result': str(e)
+        }
+    return jsonify(True)
+
+
+@app.route('/callback_view')
+def callback_view():
+    if 'result' in app.config:
+        return jsonify(app.config['result'])
+    return jsonify(False)
+
+
+@app.route('/order')
 def form_order():
-    sum_cart = request.form['sum']
-    client_id = ''
-    order_id = ''
-    service_name = ''
-    client_phone = request.form['phone']
-    client_email = request.form['email']
+    # data = PayloadData(
+    #     sum=int(request.form['sum']),
+    #     client_email=request.form['email'],
+    #     client_phone=request.form['phone'],
+    #     sign=sha256(request.form['sum'] + '' + '' + '' + request.form['phone'] + request.form['email']),
+    #     user_result_callback=url_for('callback')
+    # )
 
-    sign = sha256(sum_cart + client_id + order_id + service_name + client_phone + client_email)
+    data = PayloadData(
+        sum=int(request.args['sum']),
+        client_email=request.args['client_email'],
+        client_phone=request.args['client_phone'],
+        sign=sha256(request.args['sum'] + '' + '' + '' + request.args['client_phone'] + request.args['client_email']),
+        user_result_callback=url_for('callback'),
+    )
 
-    return sign
+    result = requests.post('https://oboi-usexpo.server.paykeeper.ru/create/', data=data)
+
+    return result.content
 
 
 def send_mail(data, mime='plain', subject='Заказ на сайте', email=app.config['email']):
